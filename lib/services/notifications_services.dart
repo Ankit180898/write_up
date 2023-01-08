949d7448666e1654fb79bee4a26ca77b6c8e5e72
Ankit Kumar <karan180898@gmail.com>
2023-01-08 22:27:39 +0530

    added local push notification services

--- lib/services/notifications_services.dart
+++ lib/services/notifications_services.dart
@@ -1,3 +1,21 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 class NotifyHelper{
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin(); //
+
+  initializeNotification() async {
+
+    final AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings("appicon");
+      final InitializationSettings initializationSettings =
+      InitializationSettings(
+      android:initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+
+    );
+
+  }
 
 }
